5f26bc86415db4ed5a8c2fa182417b8e748e9212
Dcoding15 <debrishibiswas2003@gmail.com>
2023-12-01 08:51:59 +0530

    updated 1/12/2023

--- factorial.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-read -p "Enter a number: " num
-fact=1
-for (( i=1; i<=$num; i++ ))
-do
-	(( fact*=i ))
-done
-echo "Factorial is "$fact
--- positive_negative.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo "Enter a number: "
-read n
-if [ $n -gt 0 ]; then
-  echo "This is a positive number"
-else
-  echo "This is a negative number"
-fi
